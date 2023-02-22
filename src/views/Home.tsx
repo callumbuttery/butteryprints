052ef81e3c4f9e831de8295bbc5db848033179a1
callum buttery <callumbuttery@icloud.com>
2023-02-22 23:10:08 +0000

    Feat: add name to home screen

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,9 @@
 export const Home: React.FC = () => {
-    return (
-        <div>
-            Hello world
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <div className="relative h-32 w-32 ...">
+        <h1 className="absolute left-16 lg:left-72 top-32 text-4xl w-96 tracking-widest">Callum Buttery</h1>
+      </div>
+    </div>
+  );
+};
