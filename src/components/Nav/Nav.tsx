1642702dac523b3fdcee1475cfc39ad87491fd06
callum buttery <callumbuttery@icloud.com>
2023-02-22 22:36:25 +0000

    Fix: increase x axis padding on navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -27,17 +27,17 @@ export const Navbar: React.FC = () => {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75">
+                <a className="px-6 py-2 flex items-center text-md uppercase font-bold leading-snug hover:opacity-75">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75">
+                <a className="px-6 py-2 flex items-center text-md uppercase font-bold leading-snug hover:opacity-75">
                   About
                 </a>
               </li>
               <li className="nav-item">
-                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75">
+                <a className="px-6 py-2 flex items-center text-md uppercase font-bold leading-snug hover:opacity-75">
                   Prints
                 </a>
               </li>
