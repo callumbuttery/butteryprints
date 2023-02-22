9dad1128d423214cf14e08dcbd46de4ad0af957d
callum buttery <callumbuttery@icloud.com>
2023-02-22 23:12:14 +0000

    Feat: add photography title

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,14 @@ export const Home: React.FC = () => {
   return (
     <div className="">
       <div className="relative h-32 w-32 ...">
-        <h1 className="absolute left-16 lg:left-72 top-32 text-4xl w-96 tracking-widest">Callum Buttery</h1>
+        <div className="absolute left-16 lg:left-72 top-32 text-4xl w-96">
+          <h1 className="tracking-widest">
+            Callum Buttery
+          </h1>
+          <h2 className="mt-2 tracking-widest opacity-50">
+            Photography
+          </h2>
+        </div>
       </div>
     </div>
   );
