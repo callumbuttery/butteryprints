57a683bdc6ef3c4ba25a16ef6ae19ac17379ec6d
callum buttery <callumbuttery@icloud.com>
2023-02-22 23:03:45 +0000

    Feat: nav fixes

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Icon from '@mdi/react';
+import { mdiMenu } from '@mdi/js';
 
 export const Navbar: React.FC = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -7,15 +9,15 @@ export const Navbar: React.FC = () => {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a className="text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase">
-              Navbar
+            <a className="text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase">
+              B.V
             </a>
             <button
               className="text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
+             <Icon path={mdiMenu} color='black' size={1.5}/>
             </button>
           </div>
           <div
@@ -39,6 +41,11 @@ export const Navbar: React.FC = () => {
               <li className="nav-item">
                 <a className="px-6 py-2 flex items-center text-md uppercase font-bold leading-snug hover:opacity-75">
                   Prints
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="px-6 py-2 flex items-center text-md uppercase font-bold leading-snug hover:opacity-75">
+                  Weddings
                 </a>
               </li>
             </ul>
