e0608fa81ebe91453178fab68f1e859d1ebadf2f
callum buttery <callumbuttery@icloud.com>
2023-02-23 18:04:16 +0000

    Feat: add in aos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import { About } from './views/About'
 import { Home } from './views/Home'
@@ -8,6 +10,11 @@ import { Prints } from './views/Prints'
 import { Navbar } from "./components/Nav/Nav";
 
 function App() {
+
+  useEffect(() => {
+    setTimeout(function () { AOS.init(); }, 1000);
+  }, [])
+  
   return (
     <div className="App">
       <Navbar/>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,12 +2,12 @@ export const Home: React.FC = () => {
   return (
     <div className="">
       <div className="relative h-32 w-32 ...">
-        <div className="absolute left-16 lg:left-72 top-32 text-4xl w-96">
+        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-72 top-32 text-4xl w-96">
           <h1 className="tracking-widest">
             Callum Buttery
           </h1>
           <h2 className="mt-2 tracking-widest opacity-50">
-            Photography
+            Photographer
           </h2>
         </div>
       </div>
