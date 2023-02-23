2fecc6ef1ac00af5e6be5b6b2d1bb1cf944cb1a1
callum buttery <callumbuttery@icloud.com>
2023-02-23 19:18:51 +0000

    Feat: add in background

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,8 +5,8 @@ import { mdiMenu } from '@mdi/js';
 export const Navbar: React.FC = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3">
+    <div className="">
+      <nav className="relative flex flex-wrap items-center justify-between bg-transparent drop-shadow-2xl border-none">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a className="text-4xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase">
@@ -52,6 +52,6 @@ export const Navbar: React.FC = () => {
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,12 @@
 export const Home: React.FC = () => {
   return (
-    <div className="">
-      <div className="relative h-32 w-32 ...">
-        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-72 top-32 text-4xl w-96">
-          <h1 className="tracking-widest">
+    <div className="h-screen bg-[url('assets/wallpaper.png')] bg-no-repeat bg-cover bg-center bg-fixed">
+      <div className="relative h-32 w-96 ...">
+        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-4xl font-bold w-96">
+          <h1 className="tracking-widest text-gray-200">
             Callum Buttery
           </h1>
-          <h2 className="mt-2 tracking-widest opacity-50">
+          <h2 className="mt-2 tracking-widest opacity-50 text-gray-200">
             Photographer
           </h2>
         </div>
