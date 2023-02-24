2c867e2fc44c06a482caa2eb6dc210aaf5975711
callum buttery <callumbuttery@icloud.com>
2023-02-24 18:12:31 +0000

    Feat: base abgout oage

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,5 +1,12 @@
+import me from '../assets/me.jpg'
+
 export const About: React.FC = () => {
-    return (
-        <div>This is about</div>
-    )
-}
+  return (
+    <div className="flex items-center justify-center">
+      <div className="h-56 grid grid-cols-2 gap-16 content-between border-blue-500 border-1">
+        <img src={me} className="object-contain rounded-lg"/>
+        <div>02</div>
+      </div>
+    </div>
+  );
+};
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,11 +2,11 @@ export const Home: React.FC = () => {
   return (
     <div className="h-screen bg-[url('assets/wallpaper.png')] bg-no-repeat bg-cover bg-center bg-fixed">
       <div className="relative h-32 w-10/12 ...">
-        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-5xl font-bold w-full">
-          <h1 className="tracking-widest text-gray-200">
+        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-5xl font-bold w-ful l">
+          <h1 className="tracking-widest text-gray-200 drop-shadow-2xl">
             Callum Buttery
           </h1>
-          <h2 className="mt-2 tracking-widest opacity-50 text-gray-200">
+          <h2 className="mt-2 tracking-widest opacity-50 text-gray-200 drop-shadow-2xl">
             Photographer
           </h2>
         </div>
