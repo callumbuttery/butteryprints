cbda83ee522f6d934962930b31ba0d8b20a7af41
callum buttery <callumbuttery@icloud.com>
2023-02-28 20:19:10 +0000

    Feat: text changes

--- src/components/About/InfoCard.tsx
+++ src/components/About/InfoCard.tsx
@@ -1,7 +1,9 @@
 export const InfoCard: React.FC = () => {
-    return (
-        <div className="text-center mt-10 text-3xl text-white">
-            Hello!
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="text-center mt-10 text-3xl text-white max-lg:text-xl">
+        Hello and welcome to my page!
+      </div>
+    </div>
+  );
+};
