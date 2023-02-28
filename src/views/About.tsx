4fb45faf502c76f794d419cb961db5bd00989dca
callum buttery <callumbuttery@icloud.com>
2023-02-28 19:08:22 +0000

    Feat: center about section

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -2,9 +2,9 @@ import me from '../assets/me.jpg'
 
 export const About: React.FC = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="h-56 grid grid-cols-2 gap-16 content-between border-blue-500 border-1">
-        <img src={me} className="object-contain rounded-lg"/>
+    <div className="flex items-center justify-center mt-32">
+      <div className="w-8/12 grid grid-cols-2 gap-16 content-between border-blue-500 border-1 max-md:grid-cols-1">
+        <img src={me} className="object-fit rounded-lg"/>
         <div>02</div>
       </div>
     </div>
