0ec46721e2abfdfb87a3c5ed28bee40432cb4029
callum buttery <callumbuttery@icloud.com>
2023-02-28 20:06:28 +0000

    Feat: more styling to about card

---
+++ src/components/About/InfoCard.tsx
@@ -0,0 +1,7 @@
+export const InfoCard: React.FC = () => {
+    return (
+        <div className="text-center mt-10 text-3xl text-white">
+            Hello!
+        </div>
+    )
+}
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,13 +1,14 @@
-import me from "../assets/me.jpg";
+import me from "../assets/me.jpeg";
+import { InfoCard } from "../components/About/InfoCard";
 
 export const About: React.FC = () => {
   return (
-    <div className="bg-red-500 h-screen">
-      <div className="flex items-center justify-center pt-32">
-        <div className="w-8/12 grid grid-cols-2 content-between max-md:grid-cols-1 ">
-          <img src={me} className="object-fit drop-shadow-2xl rounded-l-2xl"/>
-          <div className="bg-blue-500 drop-shadow-2xl rounded-r-2xl">
-              
+    <div className="h-screen">
+      <div className="flex items-center justify-center pt-16">
+        <div className="w-8/12 grid grid-cols-2 content-between max-md:grid-cols-1 pb-16">
+          <img src={me} className="object-fit h-6/6 drop-shadow-2xl rounded-l-2xl" />
+          <div className="bg-black drop-shadow-2xl rounded-r-2xl">
+            <InfoCard />
           </div>
         </div>
       </div>
