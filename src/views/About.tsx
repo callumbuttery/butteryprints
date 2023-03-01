4318e942fe273fca59c62d4440b2c57361d02c43
callum buttery <callumbuttery@icloud.com>
2023-03-01 22:23:51 +0000

    Fix: increase shadow on custom dropshadows

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -6,8 +6,8 @@ export const About: React.FC = () => {
     <div className="h-screen">
       <div className="flex items-center justify-center pt-16">
         <div className="w-8/12 grid grid-cols-2 content-between max-md:grid-cols-1 pb-16">
-          <img src={me} className="object-fit h-6/6 drop-shadow-2xl rounded-l-2xl" />
-          <div className="bg-black drop-shadow-2xl rounded-r-2xl">
+          <img src={me} className="object-fit h-6/6 drop-shadow-4xl rounded-l-2xl" />
+          <div className="bg-black drop-shadow-4xl rounded-r-2xl">
             <InfoCard />
           </div>
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
       dropShadow: {
         '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
         '4xl': [
-          '0 35px 35px rgba(0, 0, 0, 0.25)',
-          '0 45px 65px rgba(0, 0, 0, 0.15)'
+          '0 35px 35px rgba(0, 0, 0, 0.35)',
+          '0 45px 65px rgba(0, 0, 0, 0.25)'
         ]
       }
     },
