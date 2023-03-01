e5dff1e197f09be53e4e99c53e9a6005c3e25883
callum buttery <callumbuttery@icloud.com>
2023-03-01 21:59:51 +0000

    Fix: mobile friendly titles

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,7 @@ export const Home: React.FC = () => {
   return (
     <div className="h-screen bg-[url('assets/wallpaper.png')] bg-no-repeat bg-cover bg-center bg-fixed">
       <div className="relative h-32 w-10/12 ...">
-        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-5xl font-bold w-ful l">
+        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-5xl font-bold w-full max-lg:text-3xl">
           <h1 className="tracking-widest text-gray-200 drop-shadow-2xl">
             Callum Buttery
           </h1>
