16e704d028807f7dd037f081afe6f7f4131e1051
callum buttery <callumbuttery@icloud.com>
2023-03-01 22:22:15 +0000

    Feat: about card info

--- src/components/About/InfoCard.tsx
+++ src/components/About/InfoCard.tsx
@@ -1,8 +1,28 @@
 export default function InfoCard() {
   return (
     <div>
-      <div className="text-center mt-10 text-3xl text-white max-lg:text-xl">
-        Hello and welcome to my page!
+      <div className="text-center mt-10 text-5xl text-white max-lg:text-xl">
+        Callum Buttery
+      </div>
+
+      <div className="text-center mt-6 p-10 text-2xl text-white max-lg:text-lg">
+        Hello and welcome!
+      </div>
+
+      <div className="text-center mt-6 pr-10 pl-10 text-2xl text-white font-light max-lg:text-lg">
+        I am a photographer and content creator based in central Scotland.
+        I like to photograph almost anything, however I specialise in Wildlife & Nature.
+      </div>
+
+      <div className="text-center mt-6 pr-10 pl-10 text-2xl text-white font-light max-lg:text-lg">
+        My mission is to promote the beauty of the natural world that is all around us,
+        using my passion for photography.
+      </div>
+
+      <div className="text-center mt-6 pr-10 pl-10 text-2xl text-white font-light max-lg:text-lg pb-10">
+        I am a software Engineer by weekday, wedding photographer by weekend.
+        For any commercial opportunities, please reach out to:
+        butteryphoto@gmail.com   
       </div>
     </div>
   );
