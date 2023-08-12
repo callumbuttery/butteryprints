219c6e9435c900cea2ba59bb9c59729c728097b1
callum buttery <callumbuttery@icloud.com>
2023-08-12 21:49:25 +0100

    Feat: start featured prints section

---
+++ src/components/Prints/FeaturedPrints.tsx
@@ -0,0 +1,13 @@
+export const FeaturedPrints: React.FC = () => {
+  return (
+    <div>
+      <div className="pt-32 font-Montserrat">
+        <hr className="flex justiy-center w-64 mx-auto"></hr>
+        <h1 className="flex justify-center font-bold text-3xl mt-10 mb-10">
+          Featured Prints
+        </h1>
+        <hr className="flex justiy-center w-64 mx-auto"></hr>
+      </div>
+    </div>
+  );
+};
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,26 @@
+import { FeaturedPrints } from "../components/Prints/FeaturedPrints";
+
 export const Home: React.FC = () => {
   return (
-    <div className="h-screen bg-[url('assets/callum_buttery_kingfisher.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
-      <div className="relative h-32 w-10/12 ...">
-        <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-64 text-4xl font-bold w-full max-lg:text-3xl font-Montserrat">
-          <h1 className="tracking-widest text-gray-200 drop-shadow-2xl">
-            Callum Buttery
-          </h1>
-          <h2 className="mt-2 tracking-widest opacity-50 text-gray-200 drop-shadow-2xl">
-            Photographer
-          </h2>
+    <div>
+      <div className="h-screen bg-[url('assets/callum_buttery_kingfisher.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+        <div className="relative h-32 w-10/12 ...">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            className="absolute left-16 lg:left-48 top-64 text-4xl font-bold w-full max-lg:text-3xl font-Montserrat"
+          >
+            <h1 className="tracking-widest text-gray-200 drop-shadow-2xl">
+              Callum Buttery
+            </h1>
+            <h2 className="mt-2 tracking-widest opacity-50 text-gray-200 drop-shadow-2xl">
+              Photographer
+            </h2>
+          </div>
         </div>
+      </div>
+      <div>
+        <FeaturedPrints/>
       </div>
     </div>
   );
