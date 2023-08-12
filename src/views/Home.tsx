e260b1bfd63f739f89d19e3a6ecb2164160c0bbe
callum buttery <callumbuttery@icloud.com>
2023-08-12 20:45:15 +0100

    Fix: udpate wallpaper

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,6 @@
 export const Home: React.FC = () => {
   return (
-    <div className="h-screen bg-[url('assets/wallpaper.png')] bg-no-repeat bg-cover bg-center bg-fixed">
+    <div className="h-screen bg-[url('assets/callum_buttery_kingfisher.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
       <div className="relative h-32 w-10/12 ...">
         <div data-aos="fade-up" data-aos-duration="1000" className="absolute left-16 lg:left-48 top-32 text-5xl font-bold w-full max-lg:text-3xl">
           <h1 className="tracking-widest text-gray-200 drop-shadow-2xl">
