2f972061c67251366d360e78b221f87e94deb2fb
callum buttery <callumbuttery@icloud.com>
2023-08-19 12:12:56 +0100

    display basic featured prints

--- src/components/Prints/FeaturedPrints/FeaturedPrintsCards.tsx
+++ src/components/Prints/FeaturedPrints/FeaturedPrintsCards.tsx
@@ -1,21 +1,25 @@
-import kingfisher from '../../../assets/callum_buttery_kingfisher.jpg'
-import roedeer from '../../../assets/callum_buttery_roe_deer.jpg'
-import fox from '../../../assets/callum_buttery_fox.jpg'
+import kingfisher from "../../../assets/callum_buttery_kingfisher.jpg";
+import roedeer from "../../../assets/callum_buttery_roe_deer.jpg";
+import fox from "../../../assets/callum_buttery_fox.jpg";
 
 export const FeaturedPrintsCards: React.FC = () => {
+  const prints = [
+    { name: "Kingfisher 2023", src: kingfisher },
+    { name: "Roe Deer 2023", src: roedeer },
+  ];
 
-    const prints = [
-        {name: 'Kingfisher 2023', src: kingfisher},
-        {name: 'Fox 2023', src: fox},
-        {name: 'Roe Deer 2023', src: roedeer},
-    ]
-
-    return (
-        <div>
-            <div className="flex justify-center mt-24 columns-2">
-                <div>test 1</div>
-                <div>test 2</div>
-            </div>
+  return (
+    <div className="pb-10">
+      {prints.map((print, i) => (
+        <div className="flex justify-center mt-24 columns-2">
+          <img
+            src={print.src}
+            className="h-64 rounded-lg mr-10 flex justify-center drop-shadow-xl"
+          />
+          <div className="inline-block h-[250px] min-h-[1em] w-0.5 bg-gray-100 mr-10"></div>
+          <p>{print.name}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
