e5544cbd8f3beacaec204c1ba86658fe4aaca05d
callum buttery <callumbuttery@icloud.com>
2023-08-19 23:42:18 +0100

    Feat: start about page details

--- src/components/About/InfoSection.tsx
+++ src/components/About/InfoSection.tsx
@@ -1,7 +1,8 @@
 export const InfoSection: React.FC = () => {
     return (
-        <div>
-            
+        <div className="mt-16">
+            <div className="font-montserrat flex justify-center text-2xl mb-5">BIOGRAPHY</div>
+            <hr className="flex justify-center w-72 mx-auto"/>
         </div>
     )
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -34,7 +34,7 @@ export const Navbar: React.FC = () => {
               </li>
               <li className="nav-item hover:text-blue-500">
                 <Link className="px-6 py-2 flex items-center text-md uppercase leading-snug hover:scale-125 text-white" to='/about'>
-                  About
+                  BIOGRAPHY
                 </Link>
               </li>
               <li className="nav-item hover:text-blue-500">
