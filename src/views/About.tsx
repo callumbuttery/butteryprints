7e186de8d609012531002ab15b711f9eea3370a5
callum buttery <callumbuttery@icloud.com>
2023-08-19 23:46:41 +0100

    Feat: scroll animation

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,10 +1,15 @@
 import me from "../assets/me.jpeg";
 import { InfoSection } from "../components/About/InfoSection";
+import { ScrollAnimator } from "../components/Loading/ScrollAnimation";
 
 export const About: React.FC = () => {
   return (
     <div>
-      <div className="h-screen bg-[url('assets/IMG_9378-ai-2.jpg')] bg-no-repeat bg-cover bg-center bg-fixed" />
+      <div className="h-screen bg-[url('assets/IMG_9378-ai-2.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+        <div className="absolute inset-x-0 bottom-0">
+          <ScrollAnimator />
+        </div>
+      </div>
       <div>
         <InfoSection />
       </div>
