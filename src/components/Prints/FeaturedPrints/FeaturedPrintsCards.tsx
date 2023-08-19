f435c23cf39af150f8340b8a851a3013aef967c8
callum buttery <callumbuttery@icloud.com>
2023-08-19 14:10:22 +0100

    Feat: display featured prints

--- src/components/Prints/FeaturedPrints/FeaturedPrintsCards.tsx
+++ src/components/Prints/FeaturedPrints/FeaturedPrintsCards.tsx
@@ -4,23 +4,42 @@ import fox from "../../../assets/callum_buttery_fox.jpg";
 
 export const FeaturedPrintsCards: React.FC = () => {
   const prints = [
-    { name: "Kingfisher 2023", src: kingfisher },
-    { name: "Roe Deer 2023", src: roedeer },
+    {
+      name: "Kai The Kingfisher ~ 2023",
+      src: kingfisher,
+      description:
+        "During high water on the River Clyde, this Kingfisher moves in land on the search for a food source. On a cold crisp January morning, sitting in a hide, I had the pleaure of spectating this kingfisher diving in and out of the water for several hours.",
+    },
+    {
+      name: "Robert The Roe ~ 2021",
+      src: roedeer,
+      description:
+        "Taken during the 2021 Roe Deer Rut. I found on the edge of an open meadow, stuck between 3 Roe Bucks chasing each other around for the right to Doe's. This Roe won the battle before posing for his victory photograph.",
+    },
+    {
+      name: "Faith the Fox ~ 2023",
+      src: fox,
+      description:
+        "Everyday for a week, I found myself sitting head to toe in camouflage after work, covered in beasties, waiting outside of a fox hole for Faith & her 5 cubs to appear. The first two days I only caught a 30 second glimpse. Day three however, she put on a show and allowed me to photograph her for 15 minutes before disappearing to looking for dinner.",
+    },
   ];
 
   return (
     <div className="pb-10">
       {prints.map((print, i) => (
-        <div className="flex justify-center mt-24 columns-2">
+        <div className="flex justify-center mt-24 columns-2 sm:columns-1">
           <img
             src={print.src}
             className="h-64 rounded-lg mr-10 flex justify-center drop-shadow-xl"
           />
           <div className="inline-block h-[250px] min-h-[1em] w-0.5 bg-gray-100 mr-10"></div>
           <div className="inline-block">
-            <p className="font-bold font-Montserrat">{print.name}</p>
-            <hr className="bg-gray-100 mt-5 mb-5"/>
-            <p>{print.name}</p>
+            <p className="font-bold font-Montserrat text-lg">{print.name}</p>
+            <hr className="bg-gray-100 mt-5 mb-5" />
+            <p className="max-w-sm font-Montserrat font-bold text-sm">
+              {print?.description}
+            </p>
+            <button type="button"/>
           </div>
         </div>
       ))}
--- src/components/Prints/FeaturedPrints/FeaturedPrintsSection.tsx
+++ src/components/Prints/FeaturedPrints/FeaturedPrintsSection.tsx
@@ -3,7 +3,7 @@ import { FeaturedPrintsCards } from "./FeaturedPrintsCards";
 export const FeaturedPrintsSection: React.FC = () => {
   return (
     <div>
-      <div className="pt-32 font-Montserrat">
+      <div className="pt-16 font-Montserrat">
         <hr className="flex justiy-center w-64 mx-auto"></hr>
         <h1 className="flex justify-center font-bold text-3xl mt-10 mb-10">
           Featured Prints
