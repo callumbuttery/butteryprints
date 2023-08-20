94cf19fad783b1dfe5c7ec2d1e16ed5f710dd795
callum buttery <callumbuttery@icloud.com>
2023-08-20 16:18:05 +0100

    Feat: add the man behind the camera text

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -6,6 +6,17 @@ export const About: React.FC = () => {
   return (
     <div>
       <div className="h-screen bg-[url('assets/IMG_9378-ai-2.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+        <div className="relative h-32 w-10/12 ...">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            className="absolute left-16 lg:left-48 top-64 text-4xl w-2/6 max-lg:text-3xl font-montserrat"
+          >
+            <h1 className="tracking-widest text-gray-200 drop-shadow-2xl font-bold">
+              The man behind the camera...
+            </h1>
+          </div>
+        </div>
         <div className="absolute inset-x-0 bottom-0">
           <ScrollAnimator />
         </div>
