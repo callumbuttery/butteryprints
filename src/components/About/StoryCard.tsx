30586d69568fc90699b91f93e1000dc36f1ca5dc
callum buttery <callumbuttery@icloud.com>
2023-08-21 09:28:36 +0100

    Feat: add text to about

--- src/components/About/StoryCard.tsx
+++ src/components/About/StoryCard.tsx
@@ -3,11 +3,15 @@ import headshot1 from "../../assets/headshots/unnamed (2).jpg";
 export const StoryCard: React.FC = () => {
   return (
     <div>
-      <div className="columns-2 flex justify-center mt-16 bg-black max-w-5xl mx-auto">
+      <div className="columns-2 flex justify-center mt-16 max-w-5xl mx-auto">
         <img src={headshot1} className="h-96" />
-        <div className="ml-10 inline-block max-w-lg text-blue-500">
-            <div className="text-4xl font-montserrat font-bold text-white">Where it all began...</div>
-            <p className="mt-10">testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest</p>
+        <div className="ml-10 inline-block max-w-2xl text-blue-500">
+            <div className="text-4xl font-montserrat text-black">Where it all began...</div>
+            <p className="mt-10 font-monserrat text-gray-500">
+              My passion for wildlife & nature was instilled in me at a young age. I was fortunate enough that the house I grew up & lived in for 25 years, 
+              is on the edge of farm land. Miles upon miles of open fields, meadows, hedgerows and woodlands to explore, the perfect recipe for encountering
+              all types of british wildlife.
+            </p>
         </div>
       </div>
     </div>
