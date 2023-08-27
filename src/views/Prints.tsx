5dbf6b79f3544d4006a2e0eebed86b81dcda572b
callum buttery <callumbuttery@icloud.com>
2023-08-27 21:20:59 +0100

    Fix: update prints page

--- src/views/Prints.tsx
+++ src/views/Prints.tsx
@@ -1,5 +1,9 @@
 export const Prints: React.FC = () => {
     return (
-        <div>This is prints</div>
+        <div className="flex justify-center h-screen bg-[url('assets/callum_buttery_fox.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+            <h1 className="flex justify-center items-center tracking-widest text-6xl font-monsterret font-montserrat font-bold mb-32 hover:text-white max-sm:text-4xl">
+                COMING SOON
+            </h1>
+        </div>
     )
 }
