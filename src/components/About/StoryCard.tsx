2b2da5de50644cad23d683c2fc2e9c55acc2cab0
callum buttery <callumbuttery@icloud.com>
2023-08-28 13:58:51 +0100

    Feat: make about page more responsive

--- src/components/About/StoryCard.tsx
+++ src/components/About/StoryCard.tsx
@@ -4,13 +4,13 @@ import headshot3 from "../../assets/headshots/unnamed.jpg";
 
 export const StoryCard: React.FC = () => {
   return (
-    <div>
-      <div className="columns-2 flex justify-center mt-32 max-w-5xl mx-auto" data-aos="fade-left"
+    <div className="">
+      <div className="grid grid-cols-2 mt-32 max-w-4xl mx-auto max-md:grid-cols-1" data-aos="fade-left"
         data-aos-duration="1000">
-        <img src={headshot1} className="h-96 mr-10 rounded drop-shadow-2xl pointer-events-none" />
-        <div className="ml-10 inline-block max-w-2xl text-blue-500">
-          <div className="text-4xl font-montserrat text-black">Where it all began...</div>
-          <p className="mt-10 font-monserrat text-gray-500">
+        <img src={headshot1} className="h-96 mr-10 rounded drop-shadow-2xl pointer-events-none max-md:mx-auto" />
+        <div className="ml-10 inline-block max-w-2xl text-blue-500 max-md:ml-0">
+          <div className="text-4xl font-montserrat text-black max-md:text-2xl max-md:mx-auto max-md:text-center">Where it all began...</div>
+          <p className="mt-10 font-monserrat text-gray-500 max-md:w-96 max-md:mx-auto max-md:text-md">
             My passion for wildlife & nature was instilled in me at a young age. I was fortunate enough that the house I grew up & lived in for 25 years,
             is on the edge of farm land. Miles upon miles of open fields, meadows, hedgerows and woodlands to explore, the perfect recipe for encountering
             all types of British wildlife.
@@ -21,11 +21,11 @@ export const StoryCard: React.FC = () => {
           </p>
         </div>
       </div>
-      <div className="columns-2 flex justify-center mt-32 max-w-5xl mx-auto" data-aos="fade-right"
+      <div className="grid grid-cols-2 mt-32 max-w-4xl mx-auto max-md:grid-cols-1" data-aos="fade-right"
         data-aos-duration="1000">
-        <div className="mr-10 inline-block max-w-2xl text-blue-500">
-          <div className="text-4xl font-montserrat text-black">...The story to date...</div>
-          <p className="mt-10 font-monserrat text-gray-500">
+        <div className="mr-10 inline-block max-w-2xl text-blue-500 max-md:mr-0">
+          <div className="text-4xl font-montserrat text-black max-md:text-2xl max-md:mx-auto max-md:text-center">...The story to date...</div>
+          <p className="mt-10 font-monserrat text-gray-500 max-md:w-96 max-md:mx-auto max-md:text-md">
             Graduating from high school in 2016, I had a year out to decide what I wanted to do with my life. One day, while looking for something to do,
             I picked up my dad's camera and took it for a spin. Over the following weeks I found my addictive personality take over as I waded into
             the world of photography, I was hooked pretty quickly.
@@ -36,14 +36,14 @@ export const StoryCard: React.FC = () => {
             the world has lost to the history books - few get to experience it in this modern world.
           </p>
         </div>
-        <img src={headshot2} className="h-96 ml-10 rounded drop-shadow-2xl pointer-events-none" />
+        <img src={headshot2} className="h-96 ml-48 rounded drop-shadow-2xl pointer-events-none max-md:mx-auto" />
       </div>
-      <div className="columns-2 flex justify-center mt-32 max-w-5xl mx-auto" data-aos="fade-left"
+      <div className="grid grid-cols-2 mt-32 max-w-4xl mx-auto max-md:grid-cols-1" data-aos="fade-left"
         data-aos-duration="1000"> 
-        <img src={headshot3} className="h-96 mr-10 rounded drop-shadow-2xl pointer-events-none" />
-        <div className="ml-10 inline-block max-w-2xl text-blue-500">
-          <div className="text-4xl font-montserrat text-black">...The journey ahead</div>
-          <p className="mt-10 font-monserrat text-gray-500">
+        <img src={headshot3} className="h-96 mr-10 rounded drop-shadow-2xl pointer-events-none max-md:mx-auto" />
+        <div className="ml-10 inline-block max-w-2xl text-blue-500 max-md:ml-0">
+          <div className="text-4xl font-montserrat text-black max-md:text-2xl max-md:mx-auto max-md:text-center">...The journey ahead</div>
+          <p className="mt-10 font-monserrat text-gray-500 max-md:w-96 max-md:mx-auto max-md:text-md">
             From late 2023 onwards, I will be looking at expanding my horizons both in both adventures and business. My main goal is to get my work out
             there for the world to see, my website print store is one way to do this, however I also want to sell physical prints to people in person.
             As for the world of adventures, I still have loads of Scotland to see.
