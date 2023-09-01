48e4052ac98bb48569a67f0112dc0b33ced7da3e
callum buttery <callumbuttery@icloud.com>
2023-09-01 22:00:09 +0100

    Fix: path finder text

--- src/components/Prints/FeaturedPrints/PrintsSection.tsx
+++ src/components/Prints/FeaturedPrints/PrintsSection.tsx
@@ -1,12 +1,16 @@
 import { PrintsCards } from "./PrintsCards";
 
+import { pathFinder } from "../../../helpers/pathFinder";
+
 export const FeaturedPrintsSection: React.FC = () => {
+
+  const heading = pathFinder('/prints') ? 'Prints' : 'Featured Prints'
   return (
     <div>
       <div className="pt-16 font-montserrat">
         <hr className="flex justiy-center w-64 mx-auto"></hr>
         <h1 className="flex justify-center font-bold text-3xl mt-10 mb-10">
-          Featured Prints
+          {heading}
         </h1>
         <hr className="flex justiy-center w-64 mx-auto"></hr>
       </div>
